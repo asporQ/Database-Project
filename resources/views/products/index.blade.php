7d9a74e6a183f430a7e640f84237dedb2596e5fb
james66 <thanawat.jsrt@gmail.com>
2024-10-29 21:55:45 +0700

    hide info from guest user

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,52 +1,74 @@
 <x-app-layout>
 
-    <div class="fixed top-0 left-0 right-0 bg-gray-800 text-white p-4 hidden" id="scroll-bar">
+    <div class="fixed top-0 left-0 right-0 bg-gray-800 text-white p-4 hidden" style="z-index: 1000;" id="scroll-bar">
         <p>Special Offers and Discounts!</p>
     </div>
 
-    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 mt-16 overflow-x-hidden">
-        <div class="h-auto sm:rounded-md mt-12">
+    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 mt-0 overflow-x-hidden">
+        <div class="h-auto sm:rounded-md mt-1">
             <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 p-4">
                 @foreach ($products as $product)
                 <div class="bg-white shadow-md rounded-lg overflow-hidden flex flex-col">
-                    <div class="flex justify-end p-4">
-                        <p class="text-sm text-gray-500 font-bold">Stock: <span class="font-bold text-2xl">{{
-                                $product->stock }}</span></p>
-                    </div>
-                    <div class="flex justify-center">
-                        <img alt="{{ $product->name }}" class="h-44 w-36 object-cover rounded-md mt-2"
-                            src="{{ $product->product_photo ? asset('storage/' . $product->product_photo) : asset('storage/default_photo.png') }}">
+
+                    <div class="relative">
+                        @auth
+                        <div
+                            class="absolute top-5 right-1 w-22 h-8 flex items-center justify-center bg-yellowy text-white text-sm font-bold px-1 rounded">
+                            @if ($product->stock > 0)
+                            In Stock: <span class="font-bold text-sm">{{ $product->stock }}</span>
+                            @else
+                            <span class="text-white px-1 rounded text-sm flex items-center justify-center">Sold
+                                Out</spaan>
+                                @endif
+                        </div>
+                        @endauth
+                        <div class="flex justify-center">
+                            <div class="max-h-80 min-h-80 overflow-hidden">
+                                <img alt="{{ $product->name }}" class="w-full h-full object-cover"
+                                    src="{{ $product->product_photo ? asset('storage/' . $product->product_photo) : asset('storage/default_photo.png') }}">
+                            </div>
+                        </div>
                     </div>
 
-                    <div class="p-4 flex-1 flex flex-col justify-between overflow-hidden">
-                        <div class="text-center overflow-hidden">
-                            <h2 class="text-2xl font-semibold text-[#474543] truncate">{{ $product->name }}</h2>
-                            <p class="mt-1 text-sm text-gray-600 overflow-ellipsis overflow-hidden">{{
-                                $product->description }}</p>
-                            <p class="mt-1 text-sm text-gray-600">
-                                Price: <span class="font-bold text-[#F3B917] text-2xl">${{ $product->price }}</span>
-                            </p>
-                            @if ($product->discount && isset($product->discount->discount_percentage))
-                            <div class="mt-1 text-sm text-black">
-                                Discount: {{$product->discount->discount_percentage}}%
-                                ({{$product->discount->start_date}} - {{$product->discount->end_date}})
-                            </div>
-                            @endif
-                            <p class="mt-1 text-sm text-gray-500">Category: {{ $product->category->name }}</p>
+                    <div class="p-4 flex-1 flex flex-col justify-between overflow-hidden text-center">
+                        <h2 class="text-2xl font-semibold text-[#474543] truncate">{{ $product->name }}</h2>
+                        <p class="mt-1 text-sm text-gray-600 overflow-ellipsis overflow-hidden">{{
+                            $product->description }}</p>
+                        @auth
+                        <p class="mt-1 text-sm text-gray-600">
+                            Price: <span class="font-bold text-[#F3B917] text-2xl">${{ $product->price }}</span>
+                        </p>
+                        @if ($product->discount && isset($product->discount->discount_percentage))
+                        <div class="mt-1 text-sm text-black">
+                            Discount: {{$product->discount->discount_percentage}}%
+                            ({{$product->discount->start_date}} - {{$product->discount->end_date}})
                         </div>
+                        @endif
+                        <p class="mt-1 text-sm text-gray-500">Category: {{ $product->category->name }}</p>
+                        @endauth
                         @auth
                         <form class="add-to-cart-form mt-2" data-product-id="{{ $product->id }}"
                             style="display: inline;">
                             @csrf
+                            @if ($product->stock > 0 )
                             <div class="flex justify-center items-center mt-2">
                                 <input min="1" name="quantity" class="w-16 p-1 border rounded" type="number" value="1"
                                     max="{{ $product->stock }}">
-                                <button class="btn btn-primary font-bold ml-2 p-2 bg-yellowy text-black rounded"
+                                <button class="btn btn-primary  font-bold ml-2 p-2 bg-yellowy text-black rounded"
                                     type="submit">Add to Cart</button>
                             </div>
+                            @else
+                            <div
+                                class="flex justify-center items-center w-15 mt-2 font-bold ml-2 p-2 bg-yellowy text-black rounded">
+                                OUT OF STOCK
+                            </div>
+                            @endif
                         </form>
                         @else
-                        <p class="mt-2 text-center"><em>Please log in to add products to your cart.</em></p>
+                        <a href="{{ route('login') }}" class="mt-2 text-center bg-yellowy rounded m-2 font-bold"
+                            style="font-size: 16px;">
+                            REGISTER AND LOGIN FOR PRICE AND TO ORDER.
+                        </a>
                         @endauth
                     </div>
                 </div>
